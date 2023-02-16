988492db954eed037b730062230f63e98c8b172b
Dennis Gabriel <dennisgabr745@gmail.com>
2023-02-16 17:15:39 -0300

    Some improvements in index.js, index.html and style.css

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,44 @@
 function insert(num){
-            var numero = document.getElementById('result').innerHTML;
-            document.getElementById('result').innerHTML = numero + num;
-        }
-        function clean(){
-            document.getElementById('result').innerHTML = "";
-        }
-        function back(){
-            var result = document.getElementById('result').innerHTML;
-            document.getElementById('result').innerHTML = result.substring(0, result.length -1);
-        }
-        function calculate(){
-            var result = document.getElementById('result').innerHTML;
-            if(result){
-                document.getElementById('result').innerHTML = eval(result);
-            }else{
-                document.getElementById('result').innerHTML = "empty field..."
-            }
-        }
+    var numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+}
+function clean(){
+    document.getElementById('result').innerHTML = "";
+}
+function back(){
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1);
+}
+function calculate(){
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(result);
+    }else{
+        document.getElementById('result').innerHTML = "empty field"
+    }
+}
+
+document.addEventListener('keypress', function(e){
+    if(e.which == 13){
+       calculate();
+       e.preventDefault();
+    }
+ }, false);
+ document.addEventListener('keydown', function(e){
+    if(e.which == 8){
+       back();
+    }
+ }, false);
+
+ var pressed = document.getElementById('result');
+
+function keyPressed(event){
+    event = event || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(event) {
+    var str = keyPressed(event);
+    pressed.innerHTML += str;
+};
